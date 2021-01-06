a13ed75492f1f3ba77f0baa038708ddf7ed05872
Shadab <somdeepshadab1@gmail.com>
2021-01-06 13:57:25 +0530

    practice

---
+++ hello.py
@@ -0,0 +1,4 @@
+num1 = input('Enter the first Number? ')
+num2 = input('Enter the second NUmber? ')
+sum = int(num1) + int(num2) 
+print(f'Sum of {num1} and {num2} is {sum}')
---
+++ weight.py
@@ -0,0 +1,6 @@
+num1 = input('Enter the weight of 1st person? ')
+num2 = input('Enter the weight of 2nd person? ')
+num3 = input('Enter the weight of 3rd person? ')
+sum = float(num1) + float(num2) + float(num3)
+avg = float(sum)/3
+print(f'Sum of weight of {num1}, {num2} and {num3} is {sum} and average is {avg}')
